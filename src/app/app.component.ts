12ec742ac7a5b2ec92951f76d4571b5c90fa6bc3
OsmanYasin-Dariel <osman.yasin@dariel.co.za>
2022-04-07 08:02:28 +0200

    dropdown and calendar in modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,47 @@
 import { Component } from '@angular/core';
+import { NgbModal, NgbTimeStruct } from '@ng-bootstrap/ng-bootstrap';
 import { ServoService } from './services/servo-service/servo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  closeResult = '';
+
+  time: NgbTimeStruct = {
+    hour: 13, 
+    minute: 30,
+    second: 0
+  };
+
   title = 'astro-web';
   selectedDevice: {
-    color: string,
-    leftSnack: string,
-    name: string,
-    rightSnack: string,
+    color: string;
+    leftSnack: string;
+    name: string;
+    rightSnack: string;
   } | null = null;
 
-  constructor(public servoService: ServoService) { }
+  constructor(
+    public servoService: ServoService,
+    private modalService: NgbModal
+  ) {}
 
   setSelectedDevice(device: any) {
     // this.selectedDevice = device;
-    this.servoService.spinRightServo().then(data=>{
+    this.servoService.spinRightServo().then((data) => {
       console.log(data);
     });
+  }
+
+  schedule(content: any) {
+    console.log('hi mom');
+    this.modalService.open(content);
+  }
+
+  displayTime() {
+    console.log(this.time);
   }
 }
